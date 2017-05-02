e89839aeaecaff7997010ea12d6748ca1347cd84
Benjamin Mayer <lolgame99@gmail.com>
2017-05-02 17:12:00 +0200

    Revert "ADDED loader"
    This reverts commit e3ba2a938ff31e1fed2858d60b06fa907f51c21f.

--- jscripts/popularSeries.js
+++ jscripts/popularSeries.js
@@ -3,31 +3,31 @@ $(function() {
         method: "GET",
         url: "https://api.themoviedb.org/3/tv/popular?api_key=02315c61f82284303a120d89ce93baa4&language=de",
         beforeSend: function() {
-            $(".wrapper").html("<div class='loader'>Loading...</div>");
+            $(".wrapper").html("<h1>Loading...</h1>");
         },
         success: function(data) {
             var output = "";
-            var left = true;
-            var test = 0;
+			var left = true;
+			var test = 0;
             $.each(data.results, function(id, obj) {
-                test += 1;
-                var synopsis = $.trim(obj.overview).substring(0, 500).split(".").slice(0, -1).join(" ") + "...";
-                if (synopsis == "...") {
-                    synopsis = "Keine Synopsis vorhanden, tut uns leid!";
-                }
+				test+=1;
+				var synopsis = $.trim(obj.overview).substring(0, 500).split(".").slice(0, -1).join(" ") + "...";
+				if (synopsis=="...") {
+					synopsis="Keine Synopsis vorhanden, tut uns leid!";
+				}
                 if (left) {
-                    output += "<div class='singlePopSeries' id='leftPop'><img class='poster' src=http://image.tmdb.org/t/p/w342" + obj.poster_path + " alt='poster'>";
-                    output += "<p class='seriesFont'>" + obj.name + "</p>";
-                    output += "<p>" + synopsis + "</p></div>";
-                    left = false;
-                } else if (!left) {
-                    output += "<div class='singlePopSeries' id='rightPop'><img class='poster' src=http://image.tmdb.org/t/p/w342" + obj.poster_path + " alt='poster'>";
-                    output += "<p class='seriesFont'>" + obj.name + "</p>";
-                    output += "<p>" + synopsis + "</p></div>";
-                    left = true;
+                	output+="<div class='singlePopSeries' id='leftPop'><img class='poster' src=http://image.tmdb.org/t/p/w342"+obj.poster_path+" alt='poster'>";
+					output+="<p class='seriesFont'>"+obj.name+"</p>";
+					output+="<p>"+synopsis+"</p></div>";
+					left = false;
+                }else if (!left) {
+					output+="<div class='singlePopSeries' id='rightPop'><img class='poster' src=http://image.tmdb.org/t/p/w342"+obj.poster_path+" alt='poster'>";
+					output+="<p class='seriesFont'>"+obj.name+"</p>";
+					output+="<p>"+synopsis+"</p></div>";
+					left = true;
                 }
             });
-            $(".wrapper").html(output);
+			$(".wrapper").html(output);
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(textStatus, errorThrown);
