7b272dd2cb4ac8f18c180c68d7ae5a53a5137a58
paxetheninja <florian.wachter698@gmail.com>
2017-05-03 20:47:30 +0200

    View Series BSP Form, to put in linked data

--- jscripts/popularSeries.js
+++ jscripts/popularSeries.js
@@ -16,28 +16,18 @@ $(function() {
                     synopsis = "Keine Synopsis vorhanden, tut uns leid!";
                 }
                 if (left) {
-<<<<<<< HEAD
-                	output+="<div class='singlePopSeries' id='leftPop'><img class='poster' src=http://image.tmdb.org/t/p/w342"+obj.poster_path+" alt='poster'>";
-					output+="<p class='seriesFont'>"+obj.name+"</p>";
-					output+="<p id='synops'>"+synopsis+"</p></div>";
-					left = false;
-                }else if (!left) {
-					output+="<div class='singlePopSeries' id='rightPop'><img class='poster' src=http://image.tmdb.org/t/p/w342"+obj.poster_path+" alt='poster'>";
-					output+="<p class='seriesFont'>"+obj.name+"</p>";
-					output+="<p id='synops'>"+synopsis+"</p></div>";
-					left = true;
-=======
+
                     output += "<div class='singlePopSeries' id='leftPop'><img class='poster' src=http://image.tmdb.org/t/p/w342" + obj.poster_path + " alt='poster'>";
                     output += "<p class='seriesFont'>" + obj.name + "</p>";
-                    output += "<p>" + synopsis + "</p></div>";
+                    output += "<p id='synops'>" + synopsis + "</p></div>";
                     left = false;
                 } else if (!left) {
                     output += "<div class='singlePopSeries' id='rightPop'><img class='poster' src=http://image.tmdb.org/t/p/w342" + obj.poster_path + " alt='poster'>";
                     output += "<p class='seriesFont'>" + obj.name + "</p>";
-                    output += "<p>" + synopsis + "</p></div>";
+                    output += "<p id='synops'>" + synopsis + "</p></div>";
                     left = true;
->>>>>>> origin/master
                 }
+
             });
             $(".wrapper").html(output);
         },
