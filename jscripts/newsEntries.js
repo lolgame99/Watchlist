f4975ccffb412690bf0094b8cc5c428c6e0635eb
Benjamin Mayer <lolgame99@gmail.com>
2017-05-08 15:27:48 +0200

    ADDED airing today script

--- jscripts/newsEntries.js
+++ jscripts/newsEntries.js
@@ -1,13 +1,34 @@
 $(function() {
-	var output = "";
-	$.getJSON("resources/news.json", function(data) {
-		$.each(data.entries, function(i, obj) {
-			output+="<h4>"+obj.head+" , "+obj.date+"</h4>";
-			output+=obj.text;
-			if (!i+1==data.entries.length) {
-				output+="<div class='spacer'></div>"
-			}
-		});
-		$("#sitenews").html(output);
-	});
+    $.ajax({
+        method: "GET",
+        url: "https://api.themoviedb.org/3/tv/airing_today?api_key=02315c61f82284303a120d89ce93baa4&language=de&page=1",
+        beforeSend: function() {
+            $("#newSeries").html("<div class='loader'>Loading...</div>");
+        },
+        success: function(data) {
+            var output = "<ul>";
+            $.each(data.results, function(id, obj) {
+                output += "<li data-id='" + obj.id + "' class='seriesLink'>" + obj.name + "</li>";
+            });
+            output += "</ul>";
+            $("#newSeries").html(output);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
+
+    var output = "";
+    $.getJSON("resources/news.json", function(data) {
+        $.each(data.entries, function(i, obj) {
+            output += "<h4>" + obj.head + " , " + obj.date + "</h4>";
+            output += obj.text;
+            if (!i + 1 == data.entries.length) {
+                output += "<div class='spacer'></div>"
+            }
+        });
+        $("#sitenews").html(output);
+    });
+
+
 });
