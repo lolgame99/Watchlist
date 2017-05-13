0496d85835ce50f2c9f794c3963f4b164453e58f
Benjamin Mayer <lolgame99@gmail.com>
2017-05-13 14:04:00 +0200

    ADDED old search script

--- jscripts/seriesListSearch.js
+++ jscripts/seriesListSearch.js
@@ -40,7 +40,7 @@ $(function() {
     });
 
     $("#seriesSearchInput").keypress(function(event) {
-        search();
+        searchOld();
     });
 });
 
@@ -62,5 +62,51 @@ function search() {
             }
             return false;
         }).show();
+    }
+}
+
+function searchOld() {
+    // Declare variables 123
+    var input, filter, table, tr, td, nav, i;
+    input = document.getElementById('seriesSearchInput');
+    filter = input.value.toUpperCase();
+    table = document.getElementById("seriestable");
+    tr = table.getElementsByTagName('tr');
+
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        nav = td.getElementsByClassName("searchedEntry")[0];
+        if (nav) {
+            if (nav.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                nav.style.display = "";
+            } else {
+                nav.style.display = "none";
+            }
+        }
+    }
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        nav = td.getElementsByClassName("searchedEntry")[0];
+        if (nav) {
+            if (nav.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                nav.style.display = "";
+            } else {
+                nav.style.display = "none";
+            }
+        }
+    }
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+
+        nav = td.getElementsByClassName("searchedEntry")[0];
+        if (nav) {
+            if (nav.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                nav.style.display = "";
+            } else {
+                nav.style.display = "none";
+            }
+        }
     }
 }
